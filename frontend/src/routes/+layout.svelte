6cb8c8ab949f864b2af5592cf241d202b5b6f2ca
Igor Mamaev <igor.mamaev1@gmail.com>
2026-06-13 01:57:41 +0300

    favicon

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.jpg';
 	import { AppProvider } from '$lib/shared/providers';
 	import './layout.css';
 
